03b9b2be407b87605bf788161516be2081978049
Popiuy <rymaxpayne@gmail.com>
2023-09-27 14:56:38 -0700

    fixed about more with lists

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,10 +2,14 @@ export default function About() {
     return (
         <div>
             <h1>About Me</h1>
-            <p>Ryan Payne is a Graduate of the UC Berkeley Extension Fullstack Coding Bootcamp. Ryan worked as a soccer referee growing up.  </p>
-            <p>Worked at Cordevalle as security, valet, and server. Moved into the restaurant scene during college and quickly moved into bartending. </p>
-            <p>Started with Olive Garden and Chillis, and now im at San Diego Tennis and Raquetball Club on the weekends. </p>
-            <p>Currently looking for employment or learning oportunities in front or backend coding. </p>
+            <ul>
+                <li>Ryan Payne is a Graduate of the UC Berkeley Extension Fullstack Coding Bootcamp. </li>
+                <li>Worked as a soccer referee growing up. </li>
+                <li>Worked at Cordevalle as security, valet, and server right out of highschool. </li>
+                <li>Moved into the restaurant scene during college and quickly moved into bartending. </li>
+                <li>Started with Olive Garden and Chilis, and now im at San Diego Tennis and Raquetball Club bartending on the weekends. </li>
+                <li>Currently looking for employment or learning oportunities in front or backend coding. </li>
+            </ul>
             <p>Check out my Portfolio for some of my work!</p>
             <img src="/images/Portfolio-Headshot.png" alt="Profile Photo" />
         </div>
