b5b230fe9afb3b728424f2633c10a52523397d63
Popiuy <rymaxpayne@gmail.com>
2023-09-27 15:39:38 -0700

    fixed icons

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -3,13 +3,13 @@ export default function Footer() {
     <div className="footer">
       <h5 className="fw-semibold">Links:</h5>
       <a href="https://www.linkedin.com/in/ryan-payne-218378280/" target="_blank" rel="noopener noreferrer">
-        <img className="myimg" src="public/images/Linkedin.png" alt="linkedin icon" height="50px" width="75px" />
+        <img className="myimg" src="/images/Linkedin.png" alt="linkedin icon" height="50px" width="75px" />
       </a>
       <a href="https://github.com/Popiuy" target="_blank" rel="noopener noreferrer">
         <img className="myimg" src="/images/github.png" alt="github icon" height="50px" />
       </a>
       <a href="https://stackoverflow.com/users/22142295/ryan-payne" target="_blank" rel="noopener noreferrer">
-        <img className="myimg" src="public/images/stackoverflow.png" alt="Stack overflow icon" height="50px" />
+        <img className="myimg" src="/images/stackoverflow.png" alt="Stack overflow icon" height="50px" />
       </a>
     </div>
   );
