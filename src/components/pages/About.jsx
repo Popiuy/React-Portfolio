4af928da46c561d7a1016c27784e738aa1da29d5
Popiuy <rymaxpayne@gmail.com>
2023-09-27 14:49:03 -0700

    added to my about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
             <p>Ryan Payne is a Graduate of the UC Berkeley Extension Fullstack Coding Bootcamp. Ryan worked as a soccer referee growing up.  </p>
             <p>Worked at Cordevalle as security, valet, and server. Moved into the restaurant scene during college and quickly moved into bartending. </p>
             <p>Started with Olive Garden and Chillis, and now im at San Diego Tennis and Raquetball Club on the weekends. </p>
-            <p>Currently looking for employment or learning oportunities. </p>
+            <p>Currently looking for employment or learning oportunities in front or backend coding. </p>
             <p>Check out my Portfolio for some of my work!</p>
             <img src="/images/Portfolio-Headshot.png" alt="Profile Photo" />
         </div>
